7d92ee12bb8926e31fe873b7fd121aaea4609251
toluhunter <toluhunter19@gmail.com>
2023-04-03 00:42:52 +0100

    Added feature: adding category

--- report/urls.py
+++ report/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("add/", views.CreateReportView.as_view(), name="add-report"),
     path("fetch-rooms/", views.FetchRoomsView.as_view(), name="fetch-rooms"),
     path("manage/<uuid:id>/", views.ManageReportView.as_view(), name="manage-reports"),
+    path("add-category/", views.CreateCategoryView.as_view(), name="add-category"),
+    path("categories/", views.ListCategoryView.as_view(), name="categories"),
     path("", views.FetchReportView.as_view(), name="list-reports"),
 ]
