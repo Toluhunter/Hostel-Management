e9ec3fe9bd562826944f672b789a2cdab7b0ca21
toluhunter <toluhunter19@gmail.com>
2023-04-12 19:36:14 +0100

    Added Issue Per month

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("issues/", views.FetchAmountReport.as_view(), name="amount-issues")
+    path("issues/", views.FetchAmountReport.as_view(), name="amount-issues"),
+    path("issues-per-year/", views.FetchIssueRatioMonth.as_view(), name="issues-per-year")
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
@@ -12,5 +13,33 @@ class FetchAmountReport(generics.GenericAPIView):
         record = {}
         record["resolved"] = Report.objects.filter(resolved=True).count()
         record["unresolved"] = Report.objects.filter(resolved=False).count()
+
+        return Response(record)
+
+
+class FetchIssueRatioMonth(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def get(self, request):
+
+        record = {
+            "Jan": 0,
+            "Feb": 0,
+            "Mar": 0,
+            "Apr": 0,
+            "May": 0,
+            "Jun": 0,
+            "Jul": 0,
+            "Aug": 0,
+            "Sep": 0,
+            "Oct": 0,
+            "Nov": 0,
+            "Dec": 0
+        }
+
+        year = datetime.now().year
+        for month, num in zip(record.keys(), range(1, 13)):
+            record[month] = Report.objects.filter(
+                date_added__month=num, date_added__year=year).count()
 
         return Response(record)
